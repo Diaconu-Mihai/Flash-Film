d44be63159f946e0684b69e73a71cb7ecc51354f
Mihai <mihai_bosu8@yahoo.com>
2024-04-03 23:05:56 +0300

    first commit

---
+++ frontend/script.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function() {
+    const rootElement = document.getElementById("root");
+
+    const apiKey = `e39de4c3c1c2758867914877e0dea313`
+    const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data["results"][0]);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+})
