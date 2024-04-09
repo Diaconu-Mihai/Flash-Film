374b37a37f7fa1ab458c593ecfee2b5d89559c2f
deea8 <daescuandreea82002@gmail.com>
2024-04-09 12:54:34 +0300

    img details

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,26 @@
 window.addEventListener("load", function() {
-    //const rootElement = document.getElementById("root");
+    const rootElement = document.getElementById("root");
 
     const apiKey = `e39de4c3c1c2758867914877e0dea313`
     const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
+
+    
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            const movie = data["results"][15];
+            console.log(movie);
+
+            const posterUrl= `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+            rootElement.insertAdjacentHTML("beforeend", 
+            `<div class="movie">
+                <div class="title">${movie.title}</div>
+                <img src="${posterUrl}" class="poster">
+                </div>
+
+                <div class="description">${movie.overview}</div>
+                `)
         })
         .catch(error => {
             console.log(error);
